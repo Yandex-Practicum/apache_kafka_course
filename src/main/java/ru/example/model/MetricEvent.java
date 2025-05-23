7622bd7cda35865138422de2f066213a602a4c7d
user <user@email>
2025-05-23 16:58:21 +0300

    module 3 final task java

---
+++ src/main/java/ru/example/model/MetricEvent.java
@@ -0,0 +1,15 @@
+package ru.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MetricEvent {
+    private String name;
+    private String type;
+    private String description;
+    private Double value;
+}
