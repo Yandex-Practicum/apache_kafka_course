5dd8c95ec10bbdd020f33aab5bf963610c0c1c47
user <user@email>
2025-07-29 21:24:32 +0300

    yandex cloud go

--- 2/datanode_entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-mkdir -p /usr/local/hadoop/hdfs/datanode
-chmod -R 777 /usr/local/hadoop/hdfs/datanode
-
-exec "$@"
--- 2/namenode_entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-mkdir -p /usr/local/hadoop/hdfs/namenode
-chmod -R 777 /usr/local/hadoop/hdfs/namenode
-
-hdfs namenode -format
-exec "$@"
